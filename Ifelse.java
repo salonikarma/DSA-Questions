e2d526db0c446417998253cd30487c2ab9aa550f
salonikarma <101549960+salonikarma@users.noreply.github.com>
2023-08-19 22:56:55 +0530

    Add files via upload

---
+++ Ifelse.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Ifelse {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        if(age>18){
+            System.out.println("You are an adult");
+        }
+        else{
+            System.out.println("You are not an adult");
+        }
+    }
+}
