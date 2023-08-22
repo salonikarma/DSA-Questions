5a145c0503bd9218c7f36701865012569abebaaa
salonikarma <101549960+salonikarma@users.noreply.github.com>
2023-08-22 21:39:02 +0530

    Add files via upload

--- whileloop2.java
+++ whileloop2.java
@@ -1,5 +1,6 @@
 public class whileloop2 {
     public static void main(String args[]){
+        //print number from 1 to 10
        int counter = 1;
        while(counter <= 10){
            System.out.println(counter);
