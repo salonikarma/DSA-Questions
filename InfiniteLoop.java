6e59d7885652dbf30f8e53ee6946951efcfdcc80
salonikarma <101549960+salonikarma@users.noreply.github.com>
2023-09-04 22:17:15 +0530

    Add files via upload

---
+++ InfiniteLoop.java
@@ -0,0 +1,7 @@
+public class InfiniteLoop {
+    public static void main(String args[]) {
+        for (int i = 0; 1 < 400; i++) {
+            System.out.println("Hello world");
+        }
+    }
+}
