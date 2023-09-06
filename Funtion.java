d573c48d4029595ab5f47373a527daf72e31d340
salonikarma <101549960+salonikarma@users.noreply.github.com>
2023-09-06 22:09:29 +0530

    Add files via upload

---
+++ Funtion.java
@@ -0,0 +1,10 @@
+public class Funtion {
+    public static void printHelloworld(){
+        System.out.println("Hello world!!!");
+        System.out.println("Hello world!!!");
+        System.out.println("Hello world!!!");
+    }
+    public static void main(String args[]){
+        printHelloworld(); //Function calling
+    }
+}
