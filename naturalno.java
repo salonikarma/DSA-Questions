0fdc1b3f758c7c5c9478058c60c0f4de046d82f1
salonikarma <101549960+salonikarma@users.noreply.github.com>
2023-09-11 23:20:06 +0530

    Add files via upload

---
+++ naturalno.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class naturalno
+{
+  public static void main(String args[])
+  {
+    Scanner sc = new Scanner(System.in);
+     System.out.println("Please enter any number");
+     int n = sc.nextInt();
+    for (int i=0;i<=n;i++)
+    {
+     System.out.println(i);
+    }
+  }
+}
